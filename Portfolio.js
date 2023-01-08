ed7b67bd46a7ebb29393f66a94aafffedf5d75a0
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-01-08 19:46:08 +0530

    constructed responsive navbar

--- Portfolio.js
+++ Portfolio.js
@@ -1,2 +1,8 @@
 let a=prompt("May I know your name")
 alert("Welcome to our website "+a)
+const ham = document.querySelector('.header .nav-bar .ham');
+const nav = document.querySelector('.header .nav-bar nav');
+ham.addEventListener('click',()=>{
+    nav.classList.toggle('nav-toggle');
+    ham.classList.toggle('close');
+})
