db6796216210741b4d93c8420ae2c814ad5f6c15
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-07-19 13:16:58 +0530

    modified someelements

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 // import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
-import { Lines } from 'react-preloaders';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <React.Fragment>
     <App />
-    <Lines/>
     </React.Fragment>
   </React.StrictMode>,
 )
