8b708c3b675ffc2a535cf02c8deb879f0fcfae30
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-10-08 09:02:26 +0530

    added music

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import './index.css';
-import { Cube } from 'react-preloaders';
-import Sound from 'react-sound';
+// import { Cube } from 'react-preloaders';
+// import Sound from 'react-sound';
 import portfoliosound from "./assets/portfoliosound.mp3"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <React.Fragment>
     <App />
-    <Cube />
-    <Sound
-      url={portfoliosound}
-      playStatus={Sound.status.PLAYING}
-    />
+    <embed className='invisible' src={portfoliosound} loop="true" autostart="true" />
     </React.Fragment>
   </React.StrictMode>,
 )
