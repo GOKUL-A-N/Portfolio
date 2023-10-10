a6bf43e7c54073298b212baa8a101088ecc51117
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-10-10 18:38:28 +0530

    added music

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,29 @@ import About from './components/About';
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import portfoliosound from "./assets/portfoliosound.ogg"
+import { useState } from 'react';
+// import ReactAudioPlayer from "react-audio-player";
+
+// const [music,onmusic] = useState();
+
+const handlemusic = () => {
+  const x = document.getElementById('music')
+  // (x.play === false)? x.autoplay = true: x.play = false;
+  x.autoplay = true
+  x.load();
+}
+
 
 
 const App = () => {
   return (
     <div className=''>
       
-      <div>
+      <div className='flex-1 flex flex-row'>
         <h4 className='text1 z-10 top-[400px] rotate-[-90deg] fixed items-center right-[-70px] invisible sm:visible' >gokulnatarajmoorthy@gmail.com</h4>
+        <button className='text1 font-extrabold z-10 top-[400px] rotate-[-90deg] fixed text-white' onClick={handlemusic}>Music on</button>
+        {/* <button className='text1 z-10 top-[490px] rotate-[-90deg] fixed text-white' onClick={off}>Sound off</button> */}
       </div>
       <Navbar />
       <section id="home"><Home /></section>
@@ -21,6 +36,7 @@ const App = () => {
       <section id='skills'><Skills /></section>
       <section id='projects'><Projects /></section>
       <section id='contact'><Contact /></section>
+      <audio id="music" onLoad={handlemusic} src={portfoliosound} />
       
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import App from './App.jsx';
 import './index.css';
 // import { Cube } from 'react-preloaders';
 // import Sound from 'react-sound';
-import portfoliosound from "./assets/portfoliosound.mp3"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <React.Fragment>
     <App />
-    <embed className='invisible' src={portfoliosound} loop="true" autostart="true" />
+    
     </React.Fragment>
   </React.StrictMode>,
 )
